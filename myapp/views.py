78138f81566d4a28c0c1b3c0db7292f78908d1d9
RSsunny <sunnysharif154@gmail.com>
2025-05-10 03:10:59 +0600

    static folder add and setup setting.py file

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def myApp(request):
-    return render(request, 'home.html')
+    return render(request, 'home/home.html')
     
 
