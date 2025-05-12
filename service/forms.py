aa04db670f82f855235979e1aca5d057beed9cf9
RSsunny <sunnysharif154@gmail.com>
2025-05-12 11:35:51 +0600

    user registeration

--- service/forms.py
+++ service/forms.py
@@ -1,11 +1,16 @@
 from django import forms
+from django.core import validators
 class User_Registeration(forms.Form):
     name=forms.CharField(max_length=100)
     email=forms.EmailField()
     phone=forms.NumberInput()
     message=forms.CharField(widget=forms.Textarea())
-    image=forms.ImageField()
     password=forms.CharField(widget=forms.PasswordInput())
     confirm_password=forms.CharField(widget=forms.PasswordInput())
-    check=forms.CheckboxInput()
 
+    def clean(self):
+        clean_data=super().clean()
+        password=clean_data.get('password')
+        confirm_password=clean_data.get('confirm_password')
+        if password!=confirm_password:
+            raise forms.ValidationError("Password dos not match")
--- service/views.py
+++ service/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from service.models import Service
 from . forms import User_Registeration
+
 # Create your views here.
 def service_app(request):
     service= Service.objects.all()
-    fm=User_Registeration()
+    if request.method == "POST":
+        
+        fm=User_Registeration(request.POST)
+        if fm.is_valid():
+            print(fm.cleaned_data)
+        else:
+            print(fm.errors)
+       
+    else:
+        fm=User_Registeration()
+        print("not post")
+    
     return render(request, 'service/service_app.html',{"service":service, "form":fm})
